17a4898b5e4ad75037eaecbfa9d1ebdc2fc0530c
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-09-14 15:53:08 -0300

    Update ProdutoRepository.java

--- src/main/java/com/sweetbloom/repository/ProdutoRepository.java
+++ src/main/java/com/sweetbloom/repository/ProdutoRepository.java
@@ -3,6 +3,9 @@ package com.sweetbloom.repository;
 import com.sweetbloom.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    boolean existsById(Long id);
+
+    List<Produto> findByDisponivel();
 }
